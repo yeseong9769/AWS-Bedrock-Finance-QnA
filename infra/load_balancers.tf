562f865cff4830a732c7f08146fc3f25eefc3757
YeSeong Ha <yeseong9769@daum.net>
2024-11-18 01:24:39 +0900

    outputs.tf

--- infra/load_balancers.tf
+++ infra/load_balancers.tf
@@ -123,11 +123,3 @@ resource "aws_lb" "internal_lb" {
     Name = "docuQuery-internal-lb"
   }
 }
-
-output "internal_lb_dns_name" {
-  value = aws_lb.internal_lb.dns_name
-}
-
-output "public_lb_dns_name" {
-  value = aws_lb.public_lb.dns_name
-}
