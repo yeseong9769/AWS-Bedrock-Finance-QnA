06366f0bd22298be0abdbb8a81fde242bd25af85
YeSeong Ha <yeseong9769@daum.net>
2024-11-17 16:32:16 +0900

    changed name

--- infra/ap-northeast-2/ap-northeast-2.tf
+++ infra/ap-northeast-2/ap-northeast-2.tf
@@ -1,107 +1,107 @@
 ########## VPC ##########
-resource "aws_vpc" "doquQuery_vpc" {
+resource "aws_vpc" "docuQuery_vpc" {
   cidr_block           = "172.28.0.0/16"
   instance_tenancy     = "default"
   enable_dns_hostnames = true
 
   tags = {
-    Name = "doquQuery-vpc"
+    Name = "docuQuery-vpc"
   }
 }
 
 ########## Public Subnet ##########
-resource "aws_subnet" "doquQuery_subnet_public1" {
-  vpc_id            = aws_vpc.doquQuery_vpc.id
+resource "aws_subnet" "docuQuery_subnet_public1" {
+  vpc_id            = aws_vpc.docuQuery_vpc.id
   cidr_block        = "172.28.0.0/20"
   availability_zone = "ap-northeast-2a"
 
   tags = {
-    Name = "doquQuery-subnet-public1-ap-northeast-2a"
+    Name = "docuQuery-subnet-public1-ap-northeast-2a"
   }
 }
 
-resource "aws_subnet" "doquQuery_subnet_public2" {
-  vpc_id            = aws_vpc.doquQuery_vpc.id
+resource "aws_subnet" "docuQuery_subnet_public2" {
+  vpc_id            = aws_vpc.docuQuery_vpc.id
   cidr_block        = "172.28.16.0/20"
   availability_zone = "ap-northeast-2c"
 
   tags = {
-    Name = "doquQuery-subnet-public2-ap-northeast-2c"
+    Name = "docuQuery-subnet-public2-ap-northeast-2c"
   }
 }
 
 ########## Private Subnet ##########
-resource "aws_subnet" "doquQuery_subnet_private1" {
-  vpc_id            = aws_vpc.doquQuery_vpc.id
+resource "aws_subnet" "docuQuery_subnet_private1" {
+  vpc_id            = aws_vpc.docuQuery_vpc.id
   cidr_block        = "172.28.128.0/22"
   availability_zone = "ap-northeast-2a"
 
   tags = {
-    Name = "doquQuery-subnet-private1-ap-northeast-2a"
+    Name = "docuQuery-subnet-private1-ap-northeast-2a"
   }
 }
 
-resource "aws_subnet" "doquQuery_subnet_private2" {
-  vpc_id            = aws_vpc.doquQuery_vpc.id
+resource "aws_subnet" "docuQuery_subnet_private2" {
+  vpc_id            = aws_vpc.docuQuery_vpc.id
   cidr_block        = "172.28.132.0/22"
   availability_zone = "ap-northeast-2c"
 
   tags = {
-    Name = "doquQuery-subnet-private2-ap-northeast-2c"
+    Name = "docuQuery-subnet-private2-ap-northeast-2c"
   }
 }
 
-resource "aws_subnet" "doquQuery_subnet_private3" {
-  vpc_id            = aws_vpc.doquQuery_vpc.id
+resource "aws_subnet" "docuQuery_subnet_private3" {
+  vpc_id            = aws_vpc.docuQuery_vpc.id
   cidr_block        = "172.28.136.0/22"
   availability_zone = "ap-northeast-2a"
 
   tags = {
-    Name = "doquQuery-subnet-private3-ap-northeast-2a"
+    Name = "docuQuery-subnet-private3-ap-northeast-2a"
   }
 }
 
-resource "aws_subnet" "doquQuery_subnet_private4" {
-  vpc_id            = aws_vpc.doquQuery_vpc.id
+resource "aws_subnet" "docuQuery_subnet_private4" {
+  vpc_id            = aws_vpc.docuQuery_vpc.id
   cidr_block        = "172.28.140.0/22"
   availability_zone = "ap-northeast-2c"
 
   tags = {
-    Name = "doquQuery-subnet-private4-ap-northeast-2c"
+    Name = "docuQuery-subnet-private4-ap-northeast-2c"
   }
 }
 
 ########## IGW ##########
-resource "aws_internet_gateway" "doquQuery_igw" {
-  vpc_id = aws_vpc.doquQuery_vpc.id
+resource "aws_internet_gateway" "docuQuery_igw" {
+  vpc_id = aws_vpc.docuQuery_vpc.id
 
   tags = {
-    Name = "doquQuery-igw"
+    Name = "docuQuery-igw"
   }
 }
 
 ########## EIP ##########
-resource "aws_eip" "doquQuery_eip_ap_northeast_2a" {
+resource "aws_eip" "docuQuery_eip_ap_northeast_2a" {
   domain = "vpc"
   tags = {
-    Name = "doquQuery-eip-ap-northeast-2a"
+    Name = "docuQuery-eip-ap-northeast-2a"
   }
 }
 
 ########## NAT Gateway ##########
-resource "aws_nat_gateway" "doquQuery_nat_gateway" {
-  allocation_id = aws_eip.doquQuery_eip_ap_northeast_2a.id
-  subnet_id     = aws_subnet.doquQuery_subnet_public1.id
+resource "aws_nat_gateway" "docuQuery_nat_gateway" {
+  allocation_id = aws_eip.docuQuery_eip_ap_northeast_2a.id
+  subnet_id     = aws_subnet.docuQuery_subnet_public1.id
 
   tags = {
-    Name = "doquQuery-nat-public1-ap-northeast-2a"
+    Name = "docuQuery-nat-public1-ap-northeast-2a"
   }
 }
 
 ########## Security Group ##########
 resource "aws_security_group" "bastion_host_sg" {
   name   = "bastion-host-sg"
-  vpc_id = aws_vpc.doquQuery_vpc.id
+  vpc_id = aws_vpc.docuQuery_vpc.id
 
   ingress {
     from_port   = 22
@@ -120,7 +120,7 @@ resource "aws_security_group" "bastion_host_sg" {
 
 resource "aws_security_group" "web_server_sg" {
   name   = "web_server_sg"
-  vpc_id = aws_vpc.doquQuery_vpc.id
+  vpc_id = aws_vpc.docuQuery_vpc.id
 
   ingress {
     from_port   = 80
@@ -142,12 +142,12 @@ resource "aws_security_group" "web_server_sg" {
 resource "aws_instance" "bastion_host" {
   ami           = "ami-0c63ba386d57a6296"
   instance_type = "t3a.nano"
-  subnet_id     = aws_subnet.doquQuery_subnet_public2.id
+  subnet_id     = aws_subnet.docuQuery_subnet_public2.id
   vpc_security_group_ids = [aws_security_group.bastion_host_sg.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "doquQuery-bastion-host"
+    Name = "docuQuery-bastion-host"
   }
 }
 
@@ -155,23 +155,23 @@ resource "aws_instance" "bastion_host" {
 resource "aws_instance" "web_server_1" {
   ami           = "ami-0c63ba386d57a6296"
   instance_type = "t3a.micro"
-  subnet_id     = aws_subnet.doquQuery_subnet_private1.id
+  subnet_id     = aws_subnet.docuQuery_subnet_private1.id
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
   associate_public_ip_address = false
 
   tags = {
-    Name = "doquQuery-web-server-1"
+    Name = "docuQuery-web-server-1"
   }
 }
 
 resource "aws_instance" "web_server_2" {
   ami           = "ami-0c63ba386d57a6296"
   instance_type = "t3a.micro"
-  subnet_id     = aws_subnet.doquQuery_subnet_private2.id
+  subnet_id     = aws_subnet.docuQuery_subnet_private2.id
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
   associate_public_ip_address = false
 
   tags = {
-    Name = "doquQuery-web-server-2"
+    Name = "docuQuery-web-server-2"
   }
 }
--- infra/ap-northeast-2/route_table.tf
+++ infra/ap-northeast-2/route_table.tf
@@ -1,101 +1,101 @@
 ########## Public Route Table ##########
-resource "aws_route_table" "doquQuery_rtb_public" {
-  vpc_id = aws_vpc.doquQuery_vpc.id
+resource "aws_route_table" "docuQuery_rtb_public" {
+  vpc_id = aws_vpc.docuQuery_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.doquQuery_igw.id
+    gateway_id = aws_internet_gateway.docuQuery_igw.id
   }
 
   tags = {
-    Name = "doquQuery-rtb-public"
+    Name = "docuQuery-rtb-public"
   }
 }
 
-resource "aws_route_table_association" "doquQuery_rtb_public_association_1" {
-  subnet_id      = aws_subnet.doquQuery_subnet_public1.id
-  route_table_id = aws_route_table.doquQuery_rtb_public.id
+resource "aws_route_table_association" "docuQuery_rtb_public_association_1" {
+  subnet_id      = aws_subnet.docuQuery_subnet_public1.id
+  route_table_id = aws_route_table.docuQuery_rtb_public.id
 }
 
-resource "aws_route_table_association" "doquQuery_rtb_public_association_2" {
-  subnet_id      = aws_subnet.doquQuery_subnet_public2.id
-  route_table_id = aws_route_table.doquQuery_rtb_public.id
+resource "aws_route_table_association" "docuQuery_rtb_public_association_2" {
+  subnet_id      = aws_subnet.docuQuery_subnet_public2.id
+  route_table_id = aws_route_table.docuQuery_rtb_public.id
 }
 
 ########## Private ##########
 #################### Private 1 ####################
-resource "aws_route_table" "doquQuery_rtb_private1" {
-  vpc_id = aws_vpc.doquQuery_vpc.id
+resource "aws_route_table" "docuQuery_rtb_private1" {
+  vpc_id = aws_vpc.docuQuery_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.doquQuery_nat_gateway.id
+    gateway_id = aws_nat_gateway.docuQuery_nat_gateway.id
   }
 
   tags = {
-    Name = "doquQuery-rtb-private1-ap-northeast-2a"
+    Name = "docuQuery-rtb-private1-ap-northeast-2a"
   }
 }
 
-resource "aws_route_table_association" "doquQuery_rtb_private1_association" {
-  subnet_id      = aws_subnet.doquQuery_subnet_private1.id
-  route_table_id = aws_route_table.doquQuery_rtb_private1.id
+resource "aws_route_table_association" "docuQuery_rtb_private1_association" {
+  subnet_id      = aws_subnet.docuQuery_subnet_private1.id
+  route_table_id = aws_route_table.docuQuery_rtb_private1.id
 }
 
 #################### Private 2 ####################
-resource "aws_route_table" "doquQuery_rtb_private2" {
-  vpc_id = aws_vpc.doquQuery_vpc.id
+resource "aws_route_table" "docuQuery_rtb_private2" {
+  vpc_id = aws_vpc.docuQuery_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.doquQuery_nat_gateway.id
+    gateway_id = aws_nat_gateway.docuQuery_nat_gateway.id
   }
 
   tags = {
-    Name = "doquQuery-rtb-private2-ap-northeast-2c"
+    Name = "docuQuery-rtb-private2-ap-northeast-2c"
   }
 }
 
-resource "aws_route_table_association" "doquQuery_rtb_private2_association" {
-  subnet_id      = aws_subnet.doquQuery_subnet_private2.id
-  route_table_id = aws_route_table.doquQuery_rtb_private2.id
+resource "aws_route_table_association" "docuQuery_rtb_private2_association" {
+  subnet_id      = aws_subnet.docuQuery_subnet_private2.id
+  route_table_id = aws_route_table.docuQuery_rtb_private2.id
 }
 
 #################### Private 3 ####################
-resource "aws_route_table" "doquQuery_rtb_private3" {
-  vpc_id = aws_vpc.doquQuery_vpc.id
+resource "aws_route_table" "docuQuery_rtb_private3" {
+  vpc_id = aws_vpc.docuQuery_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.doquQuery_nat_gateway.id
+    gateway_id = aws_nat_gateway.docuQuery_nat_gateway.id
   }
 
   tags = {
-    Name = "doquQuery-rtb-private2-ap-northeast-2a"
+    Name = "docuQuery-rtb-private2-ap-northeast-2a"
   }
 }
 
-resource "aws_route_table_association" "doquQuery_rtb_private3_association" {
-  subnet_id      = aws_subnet.doquQuery_subnet_private3.id
-  route_table_id = aws_route_table.doquQuery_rtb_private3.id
+resource "aws_route_table_association" "docuQuery_rtb_private3_association" {
+  subnet_id      = aws_subnet.docuQuery_subnet_private3.id
+  route_table_id = aws_route_table.docuQuery_rtb_private3.id
 }
 
 
 #################### Private 4 ####################
-resource "aws_route_table" "doquQuery_rtb_private4" {
-  vpc_id = aws_vpc.doquQuery_vpc.id
+resource "aws_route_table" "docuQuery_rtb_private4" {
+  vpc_id = aws_vpc.docuQuery_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.doquQuery_nat_gateway.id
+    gateway_id = aws_nat_gateway.docuQuery_nat_gateway.id
   }
 
   tags = {
-    Name = "doquQuery-rtb-private2-ap-northeast-2c"
+    Name = "docuQuery-rtb-private2-ap-northeast-2c"
   }
 }
 
-resource "aws_route_table_association" "doquQuery_rtb_private4_association" {
-  subnet_id      = aws_subnet.doquQuery_subnet_private4.id
-  route_table_id = aws_route_table.doquQuery_rtb_private4.id
+resource "aws_route_table_association" "docuQuery_rtb_private4_association" {
+  subnet_id      = aws_subnet.docuQuery_subnet_private4.id
+  route_table_id = aws_route_table.docuQuery_rtb_private4.id
 }
