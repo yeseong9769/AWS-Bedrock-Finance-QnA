4a74cad731da5b6a766b6f4da9e60614b211edb6
YeSeong Ha <yeseong9769@daum.net>
2024-11-25 18:15:52 +0900

    Remove outdated backend instructions from README and update Streamlit command in instance configuration

--- README.md
+++
@@ -1,5 +0,0 @@
-# backend
-aws configure
-sudo chown -R ec2-user /app/docuQuery
-cd /app/docuQuery/backend
-uvicorn main:app --host 0.0.0.0 --port 8000 &> uvicorn.log &
--- infra/instances.tf
+++ infra/instances.tf
@@ -50,10 +50,10 @@ resource "aws_instance" "web_server_1" {
   python3 -m venv /app/venv
   source /app/venv/bin/activate
   pip install -r /app/docuQuery/frontend/requirements.txt
-  cd /app/docuQuery/frontend
+  /app/docuQuery/frontend/.streamlit
   touch /app/docuQuery/frontend/.streamlit/secrets.toml
   echo "BACKEND_URL = \"http://${aws_lb.internal_lb.dns_name}:8000\"" > /app/docuQuery/frontend/.streamlit/secrets.toml
-  streamlit run main.py --server.port 8080 --logger.level=warning &> streamlit.log &
+  streamlit run /app/docuQuery/frontend/main.py --server.port 8080 --logger.level=warning &> streamlit.log &
   EOL
 
   tags = {
